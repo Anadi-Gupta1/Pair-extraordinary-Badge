7a714d2b413778f874e3c17fb8767ee65c160641
Anadi-Gupta1 <166429589+Anadi-Gupta1@users.noreply.github.com>
2025-05-22 21:01:01 +0530

    Update main.py
    newupdate.py
    
    Co-Authored-By: anadi21-19 <212918156+anadi21-19@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 print("hello world")
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+print("hello world")
+print("that what she said ")
+>>>>>>> Stashed changes
 =======
 print("hello world")
 print("that what she said ")
